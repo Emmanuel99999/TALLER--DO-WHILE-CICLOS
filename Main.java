a49b4b9d0ca4bdde956e28738f9c2fb64d0f8cf0
canbeagle <eafcanbeagle@gmail.com>
2024-04-18 23:56:53 -0500

    finished

--- Main.java
+++ Main.java
@@ -1,28 +1,72 @@
 import java.util.Scanner;
-import java.util.ArrayList;
 
 public class Main {
     static Scanner scanner = new Scanner(System.in);
  public static void main(String[] args){
-//factorial();
+  System.out.println("ingrese el numero del punto a revisar");
+  int opcion =scanner.nextInt();
+  
+  switch (opcion) {
+    case 1:
+    factorial();
+      break;
+    case 2:
+    divisibleTres();
+    break;
 
-salariosMax();
-//divisibleTres();
+    case 3:
+    salariosMax();
+    break;
+
+    case 4:
+    calificaciones();
+    break;
+
+    case 5:
+    fibonacci();
+    break;
+
+    case 6:
+    salariosRango();
+    break;
+
+    case 7:
+    tablas();
+    break;
+
+    case 8:
+    parImpar();
+    break;
+    default:
+      break;
+  }
+
+
+
+
+
+
+
+
 
  }   
 public static void factorial(){
     System.out.println("por favor ingrese un número entero");
-    int entero = scanner.nextInt();
-    int counter = 1;
+    int numero = scanner.nextInt();
     int factorial = 1;
-    while (counter<=entero) {
-    System.out.println(counter+"X"+(counter+1)+" = "+factorial*counter);
-    factorial *= counter;
-    counter++;    
-   
+    for (int i = 1; i <= numero; i++) {
+      if (i!=numero) {
+        System.out.print(i+"x");
+        factorial *= i;
+      }
+      else{
+        System.out.print(i);
+        factorial *= i;
+      }
+        }
+    System.out.println("="+factorial);
     }
-    System.out.println("el factorial de "+entero+" es "+factorial);
-}
+
 
 public static void salariosMax(){
 float salarioAlto = 0;
@@ -67,14 +111,106 @@ public static void divisibleTres(){
   }  
 } 
 public static void calificaciones(){
-
+  char centinela='y';
+ int counter = 0;
+    do{
+    System.out.println("por favor ingrese una nota");
+    float nota = scanner.nextFloat();
+    if (nota>=3) {
+      counter++;
+      System.out.println(" ingrese Y, si desea añadir otra nota, ingrese otro caracter si desea terminar la adición");
+      centinela= scanner.next().charAt(0);    
+      scanner.nextLine();
+    }
+       
+    }
+    while (centinela=='Y'||centinela=='y');
+    System.out.println("el total de aprobados es "+counter);
 }
 
 public static void fibonacci(){
-
+  int centinela = 0;
+  while (centinela==0) {
+  System.out.println("por favor ingrese un número mayor que 20 y menor o igual a 30 para ver su componente fibonacci");
+  int numero = scanner.nextInt();
+  int fibonacci= 0;
+  int temp =1;
+  if (numero>20&&numero<=30) {
+    while (temp<=numero) {
+      System.out.print(fibonacci+" "+temp+" ");
+      fibonacci+=temp;
+      temp+=fibonacci;
+    }
+    centinela=1;
+  }
+}
 }
 
 public static void salariosRango(){
-
+  System.out.println("ingrese cuantos empleados tiene su empresa");
+  int cabezas = scanner.nextInt();
+  int categoriaUno=0; //menos de 100.000
+  int categoriaDos=0;  // entre 200.001 y 300.000
+  int categoriaTres=0; //mas de 300.000
+  int contador = 1;
+  while (contador<=cabezas) {
+    System.out.println("por favor ingrese el salario de el empleado número "+contador);
+    float temp= scanner.nextFloat();
+    if (temp<100000) {
+      categoriaUno++;
+    }
+    else if (temp>200000&&temp<=300000) {
+      categoriaDos++;
+    }
+    else if (temp>300000) {
+      categoriaTres++;
+    }
+    else{
+      System.out.println("no hay categoria asignada para este salario");
+    }
+    contador++;
+  }
+  System.out.println("el total de trabajadores en categoría 1 es "+categoriaUno);
+  System.out.println("el total de trabajadores en categoría 2 es "+categoriaDos);
+  System.out.println("el total de trabajadores en categoría 3 es "+categoriaTres);
+}
+public static void tablas(){
+  for (int i = 1; i <= 10; i++) {
+    for (int j = 1; j <= 10; j++) {
+      System.out.println(i+" X "+j+" = "+i*j);
+    }
+  }
+}
+public static void parImpar(){
+  System.out.println("por favor ingrese un numero entero");
+  int numero = scanner.nextInt();
+  int sumatoria=0;
+  if (numero%2==0) {
+    for (int i = 1; i <= numero; i++) {
+      if (i!=numero) {
+      System.out.print(i+" + ");
+      sumatoria+=i;
+      }
+      else{
+      System.out.print(i);
+      sumatoria+=i;
+      }
+    }
+    System.out.println(" = "+sumatoria+ ", es la sumatoria de "+numero);
+  }
+  else {
+    int factorial = 1;
+    for (int i = 1; i <= numero; i++) {
+      if (i!=numero) {
+        System.out.print(i+"x");
+        factorial *= i;
+      }
+      else{
+        System.out.print(i);
+        factorial *= i;
+      }
+        }
+        System.out.println("="+factorial);
+  }
 }
 }
